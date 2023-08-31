7f2637e5ce2af727b0f137cdef11036859dc279a
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-08-30 23:20:05 -0300

    feat: pacotes, variaveis e tipos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Pacote main")
 	auxiliar.Escrever()
 
+	err := checkmail.ValidateFormat("devbook@gmail.com")
+	fmt.Println(err)
 }
