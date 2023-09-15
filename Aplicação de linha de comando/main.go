cb989e6b305dd223cc1703c6c18388f8b2c46641
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-09-15 02:30:26 -0300

    feat: aplicação busca ip e servidores

--- Aplicação de linha de comando/main.go
+++ Aplicação de linha de comando/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+//EX: comandos
+//go run main.go ip --host amazon.com.br
+//go run main.go servidores --host amazon.com.br
+
+import (
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Ponto de partida")
+	aplicacao := app.Gerar()
+	if erro := aplicacao.Run(os.Args); erro != nil {
+		log.Fatal(erro)
+	}
 }
